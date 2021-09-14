9f572ae997fe5c71854c1e39fcec5b4b8fb771b1
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-09-14 15:04:46 +0900

    rootパスを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root "api/v1/home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace 'api' do
+  namespace 'api', { format: 'json' } do
     namespace 'v1' do
+      resources :home, only: [:index]
     end
   end
 end
