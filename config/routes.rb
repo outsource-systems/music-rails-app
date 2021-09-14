97657fc5534cc7b1718b796eb8fc86ad30a7abec
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-09-14 15:49:28 +0900

    root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "api/v1/home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
