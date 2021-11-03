5ddb5a8283a6449228ebaba93741d7d371edec8b
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-11-03 23:10:14 +0900

    create roles

---
+++ app/models/role.rb
@@ -0,0 +1,8 @@
+class Role < ApplicationRecord
+    enum name: {
+      admin: 'admin',
+      customer: 'customer'
+    }, _prefix: true
+    
+    validates :name, presence: true, length: { maximum: 255 }
+end
