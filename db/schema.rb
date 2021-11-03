129a037f47dad20720e441eb71ebc9167059a091
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-11-03 23:02:44 +0900

    create categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_135150) do
+ActiveRecord::Schema.define(version: 2021_11_03_135902) do
+
+  create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "creaters", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Products", type: :request do
+RSpec.describe "Categories", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
