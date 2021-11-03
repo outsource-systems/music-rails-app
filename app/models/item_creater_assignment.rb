5a4f836123a0b0d2978baddacba8fd54fb982851
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-11-03 23:22:07 +0900

    ItemCreaterAssignment

--- app/models/item_creater_assignment.rb
+++ app/models/item_creater_assignment.rb
@@ -1,4 +1,4 @@
-class ItemCreatersAssignment < ApplicationRecord
+class ItemCreaterAssignment < ApplicationRecord
   belongs_to :item_id
   belongs_to :creater_id
 end
--- db/migrate/20211103142109_create_item_creater_assignments.rb
+++ db/migrate/20211103142109_create_item_creater_assignments.rb
@@ -1,6 +1,6 @@
-class CreateItemCreatersAssignments < ActiveRecord::Migration[6.1]
+class CreateItemCreaterAssignments < ActiveRecord::Migration[6.1]
   def change
-    create_table :item_creaters_assignments do |t|
+    create_table :item_creater_assignments do |t|
       t.bigint :item_id, null: false
       t.bigint :creater_id, null: false
       t.timestamps
