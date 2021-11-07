9dcc2e8c1fc4f1251cace7d2e67fa754456d1de1
SeiyaTakahashi <takahashi.seiya.jp@gmail.com>
2021-11-07 11:06:38 +0900

    artistゲットを追加

--- app/models/creator/artist.rb
+++
@@ -1,2 +0,0 @@
-class Creator::Artist < Creator
-end
--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,5 @@ class Product < ApplicationRecord
 
   has_many :creator_product_assignments, dependent: :destroy
   has_many :creators, through: :creator_product_assignments
+  has_many :artists, -> { where(type: 'Artist') }, through: :creator_product_assignments, source: :creator
 end
